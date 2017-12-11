314c6cfe40c07792facd952a3176be4f886bdf55
louissutter <louissutter@gmail.com>
2017-12-11 12:15:51 +0100

    function to authenticate

---
+++ flickr_auth.R
@@ -0,0 +1,14 @@
+auth.flickr <- function()
+{
+  
+  load('auth.rdata')
+  myapp<-oauth_app("Flickr R-package",key= auth$key,secret= auth$key)                  #creates the app passing the key and secret
+  
+  ep<-oauth_endpoint(request="https://www.flickr.com/services/oauth/request_token"    #get authentication credentials from the API
+                     ,authorize="https://www.flickr.com/services/oauth/authorize",
+                     access="https://www.flickr.com/services/oauth/access_token")
+  
+  sig<-oauth1.0_token(ep,myapp,cache=F)                                             #creates variable with authentication credentials
+  
+  fl_sig <- sign_oauth1.0(myapp,sig)                                                #authenticate
+}
