9d9418be3641f2b1646d0928ec2084e56d7c75fb
AugustT <tomaug@ceh.ac.uk>
2018-03-15 12:43:14 +0000

    Downloader updated to allow the user to select teh maximum quality to download

--- R/downloadImages.R
+++ R/downloadImages.R
@@ -5,6 +5,7 @@
 #' @param photoSearch_results A data.frame output from \code{photoSearch}.
 #' @param licenses  Numeric vector, set the licenses you want to download images for. See \code{getLicenses}
 #' @param saveDir Character, the path where images should be saved
+#' @param max_quality Numeric 1-4 giving the maximum quality of image you want to download 1=small, 4=large
 #' 
 #' @return NULL
 #' @export
@@ -23,7 +24,8 @@
 downloadImages <-
   function(photoSearch_results,
            licenses = 7:10,          
-           saveDir = '.'){
+           saveDir = '.',
+           max_quality = 2){
     
     # get those that forefill the license requirements
     toGet <- photoSearch_results[photoSearch_results$license %in% licenses, ]
@@ -37,7 +39,9 @@ downloadImages <-
       
     }
     
-    downloadURLs <- apply(toGet[,c('url_s', 'url_m', 'url_l', 'url_o')], MARGIN = 1, FUN = biggest_url)
+    quality <- c('url_s', 'url_m', 'url_l', 'url_o')[1:max_quality]
+    
+    downloadURLs <- apply(toGet[, quality, drop = FALSE], MARGIN = 1, FUN = biggest_url)
     
     dump <- sapply(downloadURLs, FUN = function(x){
       download.file(url = x,
